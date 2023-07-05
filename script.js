a27be98727ac0ccbe5db436722a2573d36edb2c4
jazmin <129342996+76428236@users.noreply.github.com>
2023-07-05 00:09:13 -0500

    123

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
-var counter= 1;
+const btnLeft = document.querySelector(".btn-left"),
+      btnRight = document.querySelector(".btn-right"),
+      slider = document.querySelector("#slider"),
+      sliderSection = document.querySelectorAll(".slider-section");
 
-setInterval(function(){
 
-    document.getElementById('radio' + counter).checked - true;
+btnLeft.addEventListener("click", e => moveToLeft())
+btnRight.addEventListener("click", e => moveToRight())
+
+setInterval(() => {
+    moveToRight()
+}, 5000);
+
+
+let operacion = 0,
+    counter = 0,
+    widthImg = 100 / sliderSection.length;
+
+function moveToRight() {
+    if (counter >= sliderSection.length-1) {
+        counter = 0;
+        operacion = 0;
+        slider.style.transform = `translate(-${operacion}%)`;
+        slider.style.transition = "none";
+        return;
+    } 
     counter++;
-    if(counter>4) {
-        counter =1;
-    }
-}, 7000)
-
-
+    operacion = operacion + widthImg;
+    slider.style.transform = `translate(-${operacion}%)`;
+    slider.style.transition = "all ease .6s"
     
+}  
